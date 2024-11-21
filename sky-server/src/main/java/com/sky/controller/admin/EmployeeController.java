93028ec36d9087a5259a3b05c9d0ee54ec95694c
Xy <278428376@qq.com>
2024-11-21 21:00:48 +0800

    添加员工

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -75,5 +76,12 @@ public class EmployeeController {
     public Result<String> logout() {
         return Result.success();
     }
-
+    @PostMapping
+    @ApiOperation("新增员工")
+    public Result save(@RequestBody EmployeeDTO employeeDTO){
+        log.info("新增员工：{}", employeeDTO);
+        //{}用途是把后面变量动态拼接到后面
+        employeeService.save(employeeDTO);
+        return Result.success();
+    }
 }
